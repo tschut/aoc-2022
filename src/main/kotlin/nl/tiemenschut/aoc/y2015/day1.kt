8da6d226f6e03a3d0b3a36bf83274adc0b390e0c
Tiemen Schut <tschut@gmail.com>
2023-01-21 14:16:51 +0100

    Add day1 aoc-dsl style

--- src/main/kotlin/nl/tiemenschut/aoc/y2015/day1.kt
+++ src/main/kotlin/nl/tiemenschut/aoc/y2015/day1.kt
@@ -2,16 +2,23 @@ package nl.tiemenschut.aoc.y2015
 
 import nl.tiemenschut.aoc.lib.dsl.aoc
 import nl.tiemenschut.aoc.lib.dsl.day
-import nl.tiemenschut.aoc.lib.dsl.parser.AsCharSequence
+import nl.tiemenschut.aoc.lib.dsl.parser.InputParser
+
+object ElfSplitter : InputParser<List<List<Int>>> {
+    override fun parse(input: String): List<List<Int>> =
+        input.split("\n\n").map { elf -> elf.split("\n").map { it.toInt() } }
+}
 
 fun main() {
-    aoc {
+    aoc(ElfSplitter) {
         puzzle { 2022 day 1 }
 
         part1 { input ->
+            input.maxOf { elf -> elf.sum() }
         }
 
         part2 { input ->
+            input.map { it.sum() }.sortedDescending().take(3).sum()
         }
     }
 }
